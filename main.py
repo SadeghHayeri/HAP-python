8375fe77a7ab16d51967535467a5334383099fde
SadeghHayeri <hayerisadegh@gmail.com>
2018-03-19 01:32:56 +0330

    new accessories!

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ This is:
 import logging
 import signal
 
+# from pyhap.accessories.DisplaySwitch import DisplaySwitch
+# from pyhap.accessories.LightBulb import LightBulb
+# from pyhap.accessories.ShutdownSwitch import ShutdownSwitch
 from pyhap.accessories.TemperatureSensor import TemperatureSensor
+from pyhap.accessories.nodeMCUSwitch import NodeMCUSwitch
 from pyhap.accessory import Bridge
 from pyhap.accessory_driver import AccessoryDriver
 
@@ -18,28 +22,20 @@ logging.basicConfig(level=logging.INFO)
 def get_bridge():
     """Call this method to get a Bridge instead of a standalone accessory."""
     bridge = Bridge(display_name="Bridge")
-    temp_sensor = TemperatureSensor("Termometer")
-    temp_sensor2 = TemperatureSensor("Termometer2")
-    bridge.add_accessory(temp_sensor)
-    bridge.add_accessory(temp_sensor2)
 
-    # Uncomment if you have RPi module and want a LED LightBulb service on pin 16.
-    # from pyhap.accessories.LightBulb import LightBulb
-    # bulb = LightBulb("Desk LED", pin=16)
-    # bridge.add_accessory(bulb)
+    bridge.add_accessory( NodeMCUSwitch("Relay 1", URL="http://192.168.1.11", switchIndex=1) )
+    bridge.add_accessory( NodeMCUSwitch("Relay 2", URL="http://192.168.1.11", switchIndex=2) )
+    bridge.add_accessory( NodeMCUSwitch("Relay 3", URL="http://192.168.1.11", switchIndex=3) )
+    bridge.add_accessory( NodeMCUSwitch("Relay 4", URL="http://192.168.1.11", switchIndex=4) )
+
+
+
     return bridge
 
-
-def get_accessory():
-    """Call this method to get a standalone Accessory."""
-    acc = TemperatureSensor("MyTempSensor")
-    return acc
-
-
-acc = get_accessory()  # Change to get_bridge() if you want to run a Bridge.
+acc = get_bridge()  # Change to get_bridge() if you want to run a Bridge.
 
 # Start the accessory on port 51826
-driver = AccessoryDriver(acc, port=51826)
+driver = AccessoryDriver(acc, port=60000)
 # We want KeyboardInterrupts and SIGTERM (kill) to be handled by the driver itself,
 # so that it can gracefully stop the accessory, server and advertising.
 signal.signal(signal.SIGINT, driver.signal_handler)
